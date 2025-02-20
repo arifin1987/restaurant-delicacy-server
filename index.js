601d0fd5f00cc14e49d1367a865c77a20fc6437d
unknown <arifinshohidul@gmail.com>
2025-02-20 01:47:13 -0800

    user related api created

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.59h68ks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,7 +25,9 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const menuCollection = client.db("restaurantDelicacy").collection("menu");
+    const userCollection = client.db("restaurantDelicacy").collection("users");
     const reviewCollection = client.db("restaurantDelicacy").collection("reviews");
+    const cartCollection = client.db("restaurantDelicacy").collection("carts");
     // menu collection
     app.get('/menu', async (req,res)=>{
       const result =await menuCollection.find().toArray();
@@ -35,6 +37,32 @@ async function run() {
     app.get('/reviews', async(req,res)=>{
       const result =await reviewCollection.find().toArray();
       res.send(result);
+    })
+    // Carts related api
+    app.post('/carts', async(req,res)=>{
+      const item = req.body;
+      const result = await cartCollection.insertOne(item)
+      res.send(result);
+    })
+
+    app.get('/carts', async(req,res)=>{
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+    app.delete('/carts/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
+    // users api
+    app.post('/users', async(req,res)=>{
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
