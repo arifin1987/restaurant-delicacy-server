6418b02323947737777c13beb01383ea45c13dd1
unknown <arifinshohidul@gmail.com>
2025-02-22 02:53:41 -0800

    make admin api routes implemented

---
+++ __steps.js
@@ -0,0 +1,7 @@
+/**
+     * Json web token implementation:
+     * npm i jsonwebtoken
+     * const jwt = require('jsonwebtoken');
+     * write this code in the terminal to find secret key:write: node, then write require('crypto').randomBytes(64).toString('hex')
+     * Set this token to env file as ACCESS_TOKEN_SECRET =
+     */
