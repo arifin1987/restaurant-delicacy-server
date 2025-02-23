722eaca98800a2f70e860789183d23a749a89a47
unknown <arifinshohidul@gmail.com>
2025-02-23 00:38:18 -0800

    created jwt token then received it in the front end and then sent it to backend for verification using verifyToken middleware

--- __steps.js
+++ __steps.js
@@ -4,4 +4,78 @@
      * const jwt = require('jsonwebtoken');
      * write this code in the terminal to find secret key:write: node, then write require('crypto').randomBytes(64).toString('hex')
      * Set this token to env file as ACCESS_TOKEN_SECRET =
+     * // jwt related api
+         app.post('/jwt', async(req,res)=>{
+           const user = req.body;
+           const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+             expiresIn: '1h'
+           });
+           res.send({token});
+         })
+     In the frontend AuthProvider.jsx::
+     useEffect(()=>{
+      const unsubsScribe = onAuthStateChanged(auth, currentUser=>{
+        setUser(currentUser);
+        if(currentUser){
+          const userInfo = {email: currentUser.email};
+          axiosPublic.post('/jwt', userInfo)
+          .then(response=>{
+            if(response.data.token){
+              localStorage.setItem('access-token', response.data.token)
+            }
+            else {
+              localStorage.removeItem('access-token');
+            }
+          })
+        }
+     */
+    /**
+     * If we want to use this in Allusers.jsx to get users related api
+     * const AllUsers = () => {
+    // We will use user data only here that's why we don't need to create seperate hook.
+    const axiosSecure = useAxiosSecure();
+    // in cookie do not need to set this
+    const {data: users=[], refetch} = useQuery({
+        queryKey: ['users'],
+        queryFn: async ()=>{
+            const result = await axiosSecure.get('/users', {
+              
+              headers: {
+                authorization: `Bearer ${localStorage.getItem('access-token')}`
+              }
+            })
+            return result.data;
+
+        }
+       
+
+    })
+     * 
+     */
+    /**
+     * Now we need to verifyToken in the server side
+     * // middlewares
+         const verifyToken = (req,res, next)=>{
+           console.log('inside verify token', req.headers);
+           if(!req.headers.authorization){
+             return res.status(401).send({message: 'forbidden access'});
+           }
+           const token = req.headers.authorization.split(' ')[1];
+           jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
+             if(error){
+               return res.status(401).send({message: 'forbidden access'})
+             }
+             req.decoded = decoded;
+             next();
+           })
+         }
+     
+     
+         // users related api
+         app.get('/users',verifyToken, async(req,res)=>{
+           console.log(req.headers.authorization)
+           const result = await userCollection.find().toArray();
+           
+           res.send(result);
+         })
      */
--- index.js
+++ index.js
@@ -29,6 +29,73 @@ async function run() {
     const userCollection = client.db("restaurantDelicacy").collection("users");
     const reviewCollection = client.db("restaurantDelicacy").collection("reviews");
     const cartCollection = client.db("restaurantDelicacy").collection("carts");
+
+
+    // jwt related api
+    app.post('/jwt', async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      });
+      res.send({token});
+    })
+    // middlewares
+    const verifyToken = (req,res, next)=>{
+      console.log('inside verify token', req.headers);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'forbidden access'});
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
+        if(error){
+          return res.status(401).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+
+    // users related api
+    app.get('/users',verifyToken, async(req,res)=>{
+      console.log(req.headers.authorization)
+      const result = await userCollection.find().toArray();
+      
+      res.send(result);
+    })
+    app.post('/users', async(req,res)=>{
+      const user = req.body;
+      // insert email if user doesn't exists:
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send ({message: 'user already exists', insertedId: null})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+
+    })
+    
+
+    app.delete('/users/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+      $set: {
+        role: 'admin'
+      },
+    };
+    const result = await userCollection.updateOne(filter, updateDoc);
+    res.send(result);
+    })
+
     // menu collection
     app.get('/menu', async (req,res)=>{
       const result =await menuCollection.find().toArray();
@@ -58,42 +125,9 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
-    // users related api
-    app.post('/users', async(req,res)=>{
-      const user = req.body;
-      // insert email if user doesn't exists:
-      const query = {email: user.email}
-      const existingUser = await userCollection.findOne(query);
-      if(existingUser){
-        return res.send ({message: 'user already exists', insertedId: null})
-      }
-      const result = await userCollection.insertOne(user);
-      res.send(result);
+    
 
-    })
-    app.get('/users', async(req,res)=>{
-      const result = await userCollection.find().toArray();
-      res.send(result);
-    })
-
-    app.delete('/users/:id', async(req,res)=>{
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const result = await userCollection.deleteOne(query);
-      res.send(result);
-    })
-
-    app.patch('/users/admin/:id', async(req,res)=>{
-      const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
-      const updateDoc = {
-      $set: {
-        role: 'admin'
-      },
-    };
-    const result = await userCollection.updateOne(filter, updateDoc);
-    res.send(result);
-    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
